5df7cd35297aee95226ba4bb9ee660fdbf4f5089
Gautam104 <gautamshukla263@gmail.com>
2025-05-27 18:49:29 +0530

    featur promo page added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Hero from "../layouts/home/Hero";
+import Overview from "../layouts/home/Overview";
+import KeyFeatures from "../layouts/home/FeatureCard";
+import FeaturePromo from "../layouts/home/FeaturePromo";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Overview />
+      <KeyFeatures />
+      <FeaturePromo />
     </div>
   );
 };
