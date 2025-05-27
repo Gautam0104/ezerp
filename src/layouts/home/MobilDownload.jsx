666f17a1bb177987a0ee6799dcdc30297979ecc5
Gautam104 <gautamshukla263@gmail.com>
2025-05-27 12:10:34 +0530

    proper architecture created

---
+++ src/layouts/home/MobilDownload.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MobilDownload = () => {
+  return <div></div>;
+};
+
+export default MobilDownload;
---
+++ src/layouts/home/PlansPricing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlansPricing = () => {
+  return <div></div>;
+};
+
+export default PlansPricing;
