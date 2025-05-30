410783ccb41b61a57e052a287ec8f05c6c891fc4
Gautam104 <gautamshukla263@gmail.com>
2025-05-30 15:31:11 +0530

    browserrouter change to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import AOS from "aos";
 import "aos/dist/aos.css";
